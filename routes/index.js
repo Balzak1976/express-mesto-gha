9ba40faf6690be69840a4d4f8c8f7c69b3e447e5
Max Skorokhodov <skormaksim@mail.ru>
2023-04-15 22:08:33 +0300

    feat: создать мидлвэр обработки ошибок

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,12 @@ const routes = require('express').Router();
 
 const userRoutes = require('./users');
 const cardsRoutes = require('./cards');
+const { doesUrlExist, handleErrors } = require('../middlewares/errors');
 
 routes.use('/users', userRoutes);
 routes.use('/cards', cardsRoutes);
-routes.use('*', (req, res) => {
-  res.status(404).send({ message: 'По указанному url ничего нет' });
-});
+routes.use(doesUrlExist);
 
-routes.use((err, req, res, next) => {
-  const { statusCode = 500, message, name } = err;
-  console.log('errName: ', name);
-
-  res.status(statusCode).send({
-    message: statusCode === 500
-      ? 'На сервере произошла ошибка'
-      : message,
-  });
-
-  next();
-});
+routes.use(handleErrors);
 
 module.exports = routes;
